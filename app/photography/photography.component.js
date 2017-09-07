7078c439b653b2f8824edcb09a7f0cd752bf2fe1
Winona Wixson <winonawixson@Winonas-MacBook-Air.local>
2017-09-07 17:16:38 -0400

    playing around with angular material cards

--- app/photography/photography.component.js
+++ app/photography/photography.component.js
@@ -6,7 +6,11 @@ angular.
     templateUrl: 'photography/photography.template.html',
     controller: ['$routeParams',
       function PhotographyController() {
-        this.pizza = "pizza";
+        this.imagePath = "img/pinkFlowersAndBees.jpg";
+        this.imageTitle = "Fuzzy Bees";
+        this.imageDescription = "The titles of Washed Out's breakthrough song and the first " + 
+                                "single from Paracosm share the two most important words in " + 
+                                "Ernest Greene's musical language: feel it. It's a simple request, as well...";
       }
     ]
   });
